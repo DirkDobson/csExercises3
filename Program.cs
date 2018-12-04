499ce2977521dd771c6fa0b9bf3453cf33e4acee
Dirk Dobson <dirkvancouver@live.com>
2018-12-03 19:19:08 -0700

    question 3 attempt

--- Program.cs
+++ Program.cs
@@ -2,9 +2,10 @@
 
 namespace csExercises3
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        
+        public void Question1()
         {
             //Question 1
             Console.WriteLine("Put hyphen separated numbers and I see if they are consecutive");
@@ -23,24 +24,58 @@ namespace csExercises3
             }
             if(truth == true) 
                 System.Console.WriteLine("Consecutive");
+        }
 
+        public void Question2()
+        {
             //Question 2
             Console.WriteLine("Put hyphen separated numbers and I see if there are duplicates");
             var dupl = Console.ReadLine();
-            int [] dupls = Array.ConvertAll(consec.Split("-"), int.Parse);
+            int [] dupls = Array.ConvertAll(dupl.Split("-"), int.Parse);
             Array.Sort(dupls);
-            truth = true;
-            for (byte i = 0; i <= dupls.Length - 1; i++)
+            var truth1 = true;
+            for (byte i = 0; i <= dupls.Length - 2; i++)
             {
                 if(dupls[i] == dupls[i + 1])
                 {
                     System.Console.WriteLine("Duplicates Exist");
-                    truth = false;
+                    truth1 = false;
                     break;
                 }
             }
-            if(truth == true)
+            if (truth1 == true)
                 System.Console.WriteLine("No Duplicates");
+        }
+        public void Question3()
+        {
+            //Question 3
+            System.Console.WriteLine("Enter a time (HH:mm) and I will see if it exists");
+            string user = Console.ReadLine();
+            
+           if (Convert.ToBoolean(Convert.ToDateTime(user)) == true)
+           { 
+               DateTime time = Convert.ToDateTime(user);
+               if (Convert.ToBoolean(time.ToString("HH:mm")) == true)
+               {
+                   System.Console.WriteLine("Valid");
+               } else {
+                   System.Console.WriteLine("Not Valid");
+               }
+           } else {
+               System.Console.WriteLine("Not Valid");
+           }
+        }
+        public class test
+        {
+            static void Main(string[] args)
+            {
+                var display = new Program();
+                // display.Question1();
+                // display.Question2();
+                display.Question3();
+                // var now = DateTime.Now;
+                // System.Console.WriteLine(now.ToString("HH:mm"));
+            }
         }
     }
 }
